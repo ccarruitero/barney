fd6b6353e395630eb608a2e28f5e18cc531cde2f
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-10-15 15:15:47 -0500

    use test/help

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,25 +1,11 @@
 ENV['RAILS_ENV'] = 'test'
-
 require File.expand_path '../../config/environment', __FILE__
-
-require 'active_support/test_case'
-
-begin
-  require 'turn'
-  Turn.config { |c| c.natural = true }
-rescue LoadError
-end
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  include ActiveRecord::TestFixtures
+  fixtures :all
 
-  self.fixture_path = "#{Rails.root}/test/fixtures/"
-
-  # Run your tests like a man.
   def self.test_order
     :random
   end
-end
-
-class ModelTest < ActiveSupport::TestCase
 end
