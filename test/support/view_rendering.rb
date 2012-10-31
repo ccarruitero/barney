9469e0b3634ef37fa30903234b90307fd5d449aa
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-10-31 18:25:37 -0500

    add render_views option

--- test/support/view_rendering.rb
+++ test/support/view_rendering.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/class/attribute'
+
 module ActiveSupport
   module Testing
     module ViewRendering
@@ -6,25 +8,33 @@ module ActiveSupport
       attr_accessor :controller
 
       included do
+        # Set this to true if you want to render views in controller testing
+        class_attribute :render_views
+        self.render_views = false
+
         setup do
-          @_empty_view_path_set_delegator = EmptyTemplatePathSetDecorator.new(controller.class.view_paths)
-          controller.class.view_paths = ::ActionView::PathSet.new.push(@_empty_view_path_set_delegator)
-          controller.extend EmptyTemplates
+          if !render_views
+            @_empty_view_path_set_delegator = EmptyTemplatePathSetDecorator.new controller.class.view_paths
+            controller.class.view_paths = ::ActionView::PathSet.new.push @_empty_view_path_set_delegator
+            controller.extend EmptyTemplates
+          end
         end
 
         teardown do
-          controller.class.view_paths = @_empty_view_path_set_delegator.original_path_set
+          if !render_views
+            controller.class.view_paths = @_empty_view_path_set_delegator.original_path_set
+          end
         end
       end
 
       class EmptyTemplatePathSetDecorator < ::ActionView::Resolver
         attr_reader :original_path_set
 
-        def initialize(original_path_set)
+        def initialize original_path_set
           @original_path_set = original_path_set
         end
 
-        def find_all(*args)
+        def find_all *args
           original_path_set.find_all(*args).collect do |template|
             ::ActionView::Template.new(
               '',
@@ -40,17 +50,17 @@ module ActiveSupport
       end
 
       module EmptyTemplates
-        def prepend_view_path(new_path)
+        def prepend_view_path new_path
           lookup_context.view_paths.unshift(*_path_decorator(new_path))
         end
 
-        def append_view_path(new_path)
+        def append_view_path new_path
           lookup_context.view_paths.push(*_path_decorator(new_path))
         end
 
         private
 
-        def _path_decorator(path)
+        def _path_decorator path
           EmptyTemplatePathSetDecorator.new(ActionView::PathSet.new(Array.wrap(path)))
         end
       end
