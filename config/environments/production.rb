51d00e1fc11aaed48e856226eb46eb8c8fcf7120
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-11-12 16:39:19 -0500

    eager load lib

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Barney::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Eager load lib directory.
+  config.eager_load_paths += %W(#{config.root}/lib)
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
