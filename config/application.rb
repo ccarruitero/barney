47b2db649d5a6edffddc13aa98095a59ac121d14
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-01-14 13:21:02 -0500

    oops remove AR/PITA T_T

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'sprockets/railtie'
-require 'active_record/pita'
 
 if defined? Bundler
   # If you precompile assets before deploying to production, use this line
