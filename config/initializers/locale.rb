ec74472446561f18a1071c4fc239b61a4db8b823
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-01-17 01:49:40 -0500

    :scissors: whitespace

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -5,5 +5,5 @@
 # Rails.application.config.time_zone = 'Central Time (US & Canada)'
 
 # The default locale is :en and all translations from config/locales/**/*.rb,yml are auto loaded.
- Rails.application.config.i18n.load_path += Dir[ Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s ]
+Rails.application.config.i18n.load_path += Dir[ Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s ]
 # Rails.application.config.i18n.default_locale = :de
