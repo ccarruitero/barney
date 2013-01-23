12b976b6af56b7f686c866f5b44fd042bcfc26cc
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-01-23 14:05:32 -0500

    dont autoload lib

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module Barney
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib)
-
     # Generators config.
     config.generators do |g|
       g.assets         false
