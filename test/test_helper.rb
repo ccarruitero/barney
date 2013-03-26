6f031cd42582f897d3a2155759b1d6c6573652d8
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-03-26 17:03:27 -0500

    require minitest-focus

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path '../../config/environment', __FILE__
 require 'rails/test_help'
 require 'minitest/pride'
+require 'minitest/focus'
 require 'support/mock_and_stub_model'
 require 'support/view_rendering'
 
