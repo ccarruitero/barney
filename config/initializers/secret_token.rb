6818894dfc243c763dc02f3c23392af439d224e9
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-04-01 18:35:37 -0500

    SECRET_TOKEN variable should be required in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Barney::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || "0221443db7d7e67d76be132cc138d9ed948099cf16be779fea682cd95a2b7ab35bb5fe6b94e92a1c91eabf7a6101a4cb39bc65824a68e6e89bb2e2571947ed46"
+Barney::Application.config.secret_key_base = Rails.env.production?? ENV['SECRET_TOKEN'] : "0221443db7d7e67d76be132cc138d9ed948099cf16be779fea682cd95a2b7ab35bb5fe6b94e92a1c91eabf7a6101a4cb39bc65824a68e6e89bb2e2571947ed46"
