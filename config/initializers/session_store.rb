8070f28239116d6a83b5524b53b4f21b80affad2
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-04-01 17:12:01 -0500

    encrypted_cookie_store option was removed. Use cookie_store instead

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Barney::Application.config.session_store :encrypted_cookie_store, key: '_barney_session'
+Barney::Application.config.session_store :cookie_store, key: '_barney_session'
