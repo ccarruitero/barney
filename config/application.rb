9407dc568582b76ebe52574e7d7e15c8efcf400a
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-04-29 14:29:07 -0500

    :haircut:

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path '../boot', __FILE__
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require :default, Rails.env
 
 module Barney
   class Application < Rails::Application
