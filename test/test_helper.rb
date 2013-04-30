0acf48551343353b723b7103951738673dcd0b1e
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-04-29 20:03:44 -0500

    run to the hills

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path '../../config/environment', __FILE__
 require 'rails/test_help'
 require 'minitest/pride'
 require 'minitest/focus'
+require 'minitest/hell'
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
